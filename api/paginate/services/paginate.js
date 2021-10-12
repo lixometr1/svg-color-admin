52f8baf16d8d582c72f6a103f1a0287fbf0087d6
Yaroslav <lixometr@gmail.com>
2021-10-12 17:49:01 +0300

    fix pagination

--- api/paginate/services/paginate.js
+++ api/paginate/services/paginate.js
@@ -2,7 +2,7 @@ module.exports = {
   async paginate(model, ctx, query, populate) {
     const perPage = ctx.query.perPage ? parseInt(ctx.query.perPage) : 15;
     const page = ctx.query.page || 1;
-    const entities = await model.find(
+    const entities = await strapi.services[model].find(
       {
         _sort: "displayOrder:desc",
         _start: (page - 1) * perPage,
@@ -12,11 +12,11 @@ module.exports = {
       },
       populate
     );
-    const total = await model.count(query);
+    const total = await strapi.services[model].count(query);
     const totalPages = Math.ceil(total / perPage);
     return {
       items: entities.map((entity) =>
-        sanitizeEntity(entity, { model: strapi.models.pattern })
+        sanitizeEntity(entity, { model: strapi.models[model] })
       ),
       meta: {
         total,

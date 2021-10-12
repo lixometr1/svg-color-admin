ee9bb3a7fbe9fc64b76703f7d7f380ab75e82b5a
Yaroslav <lixometr@gmail.com>
2021-10-12 17:07:58 +0300

    fix mysql

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: "bookshelf",
       settings: {
-        client: "mysql2",
+        client: "mysql",
         host: env("DATABASE_HOST", "localhost"),
         port: env("DATABASE_PORT", 3306),
         database: env("DATABASE_NAME", "default"),

0c438c1fd9c27b6be3cdd8e68a26b538de1adf03
Yaroslav <lixometr@gmail.com>
2021-10-13 12:32:39 +0300

    fix prod

--- config/database.js
+++ config/database.js
@@ -1,19 +1,22 @@
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "mysql",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME", "default"),
-        username: env("DATABASE_USERNAME", "root"),
-        password: env("DATABASE_PASSWORD", ""),
-      },
-      options: {
-        useNullAsDefault: true,
+module.exports = ({ env }) => {
+  console.log(env('DATABASE_HOST'), env('DATABASE_PASSWORD'))
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "mysql",
+          host: env("DATABASE_HOST", "localhost"),
+          port: env("DATABASE_PORT", 3306),
+          database: env("DATABASE_NAME", "default"),
+          username: env("DATABASE_USERNAME", "root"),
+          password: env("DATABASE_PASSWORD", ""),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
       },
     },
-  },
-});
+  };
+};
